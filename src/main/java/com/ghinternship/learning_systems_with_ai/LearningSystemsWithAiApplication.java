56d00ea3d52919f52d28e2157dde23aa812f2d9c
MeerimBat <merba971@gmail.com>
2026-03-20 13:24:15 +0100

    First Spring Boot project commit

---
+++ src/main/java/com/ghinternship/learning_systems_with_ai/LearningSystemsWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.ghinternship.learning_systems_with_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSystemsWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSystemsWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghinternship/learning_systems_with_ai/LearningSystemsWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghinternship.learning_systems_with_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSystemsWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
