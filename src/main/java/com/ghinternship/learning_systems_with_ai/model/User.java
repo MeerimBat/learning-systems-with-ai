3f444ccc12ecfa396cb9d297b1a9f103c6682505
MeerimBat <merba971@gmail.com>
2026-03-21 17:06:11 +0100

    User entity

---
+++ src/main/java/com/ghinternship/learning_systems_with_ai/model/User.java
@@ -0,0 +1,21 @@
+package com.ghinternship.learning_systems_with_ai.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 40)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String passwordHash;
+
+
+}
