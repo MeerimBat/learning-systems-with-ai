a1d742eb1c2c5fa853775db32ccbd05c95d3658b
MeerimBat <merba971@gmail.com>
2026-03-23 23:25:38 +0100

    User repository

---
+++ src/main/java/com/ghinternship/learning_systems_with_ai/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ghinternship.learning_systems_with_ai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ghinternship.learning_systems_with_ai.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
