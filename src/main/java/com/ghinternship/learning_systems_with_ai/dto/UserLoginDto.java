4f8f75a71dafa94ac0387140a05eb3cc7c227950
MeerimBat <merba971@gmail.com>
2026-04-01 22:34:42 +0200

    User LoginDto

---
+++ src/main/java/com/ghinternship/learning_systems_with_ai/dto/UserLoginDto.java
@@ -0,0 +1,25 @@
+package com.ghinternship.learning_systems_with_ai.dto;
+
+public class UserLoginDto {
+    private String username;
+    private String password;
+
+    UserLoginDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
