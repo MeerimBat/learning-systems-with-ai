8e047164994d8c407bd1fc5f961366cadd80f45d
MeerimBat <merba971@gmail.com>
2026-04-05 14:10:02 +0200

    Import dependencies

--- src/test/java/com/ghinternship/learning_systems_with_ai/AuthControllerTest.java
+++ src/test/java/com/ghinternship/learning_systems_with_ai/AuthControllerTest.java
@@ -1,4 +1,22 @@
 package com.ghinternship.learning_systems_with_ai;
 
+import com.ghinternship.learning_systems_with_ai.dto.UserRegistrationDto;
+import com.ghinternship.learning_systems_with_ai.model.User;
+import com.ghinternship.learning_systems_with_ai.repository.UserRepository;
+import org.junit.jupiter.api.MediaType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+
 public class AuthControllerTest {
 }
