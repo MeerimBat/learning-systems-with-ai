11801c289029d6b8d52ee24b47def7a8530a4763
MeerimBat <merba971@gmail.com>
2026-04-05 21:52:10 +0200

    Changed Lombok setting

--- src/main/java/com/ghinternship/learning_systems_with_ai/dto/UserRegistrationDto.java
+++ src/main/java/com/ghinternship/learning_systems_with_ai/dto/UserRegistrationDto.java
@@ -1,10 +1,10 @@
 package com.ghinternship.learning_systems_with_ai.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Getter;
+//import lombok.Setter;
 
-@Setter
-@Getter
+//@Setter
+//@Getter
 public class UserRegistrationDto {
     private String username;
     private String password;
@@ -12,4 +12,20 @@ public class UserRegistrationDto {
     UserRegistrationDto() {
     }
 
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
