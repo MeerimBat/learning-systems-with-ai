a12e50c3d9354c55c32abdd9609f04bd24e55668
MeerimBat <merba971@gmail.com>
2026-04-12 23:24:07 +0200

    Minor changes

--- src/main/java/com/ghinternship/learning_systems_with_ai/service/UserService.java
+++ src/main/java/com/ghinternship/learning_systems_with_ai/service/UserService.java
@@ -4,7 +4,7 @@ import com.ghinternship.learning_systems_with_ai.dto.UserRegistrationDto;
 import com.ghinternship.learning_systems_with_ai.model.User;
 
 public interface UserService {
-    User registerUser(UserRegistrationDto userData);
+    static User registerUser(UserRegistrationDto userData);
 
     User findByUsername(String username);
 }
