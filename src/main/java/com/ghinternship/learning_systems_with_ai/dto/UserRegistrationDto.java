9d10becbf988dec7947d92e6318a80ac05030935
MeerimBat <merba971@gmail.com>
2026-05-16 22:18:02 +0200

    Add annotations @NotBlank,@Size

--- src/main/java/com/ghinternship/learning_systems_with_ai/dto/UserRegistrationDto.java
+++ src/main/java/com/ghinternship/learning_systems_with_ai/dto/UserRegistrationDto.java
@@ -1,12 +1,24 @@
 package com.ghinternship.learning_systems_with_ai.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 //import lombok.Getter;
 //import lombok.Setter;
 
+
 //@Setter
 //@Getter
+
 public class UserRegistrationDto {
+    @NotBlank(message = "Username is required")
+    @Size(min = 6, max = 20,
+          message = "Username must be between 6 and 20 characters")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8,
+          message = "Password must be at least 8 characters long")
     private String password;
 
     public UserRegistrationDto() {
